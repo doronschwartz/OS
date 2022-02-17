77e437127456028b91d63e5adff7e226be87409e
doronschwartz <deschwa2@mail.yu.edu>
2022-02-16 22:28:40 -0500

    updates

--- Shell/tsh.c
+++ Shell/tsh.c
@@ -229,8 +229,23 @@ int parseline(const char *cmdline, char **argv)
  * builtin_cmd - If the user has typed a built-in command then execute
  *    it immediately.  
  */
+//complete
 int builtin_cmd(char **argv) 
 {
+    if(!strcmp(argv[0],"quit")){
+        exit(0);
+    }
+    else if(!strcmp(argv[0],"jobs")){
+        listjobs(jobs);
+    }
+    else if(!strcmp(argv[0],"fg")){
+        do_bgfg(argv);
+        return 1;
+    }
+     else if(!strcmp(argv[0],"bg")){
+        do_bgfg(argv);
+        return 1;
+    }
     return 0;     /* not a builtin command */
 }
 
